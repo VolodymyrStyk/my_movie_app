3d71670029c11ac2ed9d36dbde73d6aabfad8e8e
VolodymyrStyk <volodymyr.styk@gmail.com>
2021-06-14 23:52:25 +0300

    fix package.json add material UI

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
 import './App.css';
-import {MoviesList} from "./components/MoviesList";
-import {Header} from "./components/Header";
+import {MoviesList} from "./backUp/components/MoviesList";
+import {Header} from "./backUp/components/Header";
 import {
     BrowserRouter as Router,
     Route, Switch
 } from "react-router-dom";
-import {Movie} from "./components/Movie";
+import {Movie} from "./backUp/components/Movie";
 
 function App() {
     return (
         <div className="App">
-            <Router>
-                    <Header/>
-                <Switch>
-                    <Route exact path={'/movies'} component = {MoviesList}/>
-                    <Route path={`/movies/:id`} component = {Movie}/>
-                </Switch>
-            </Router>
+            sdsd
         </div>
     );
 }
 
 export default App;
+
+
+//function App() {
+//     return (
+//         <div className="App">
+//             <Router>
+//                     <Header/>
+//                 <Switch>
+//                     <Route exact path={'/movies'} component = {MoviesList}/>
+//                     <Route path={`/movies/:id`} component = {Movie}/>
+//                 </Switch>
+//             </Router>
+//         </div>
+//     );
+// }
+//
+// export default App;
