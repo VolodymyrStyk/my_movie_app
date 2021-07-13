5c11a23a77a8de95a5b97f8bdf6bf9f9cd96372c
VolodymyrStyk <volodymyr.styk@gmail.com>
2021-07-14 01:00:29 +0300

    update version

--- src/components/App.js
+++ src/components/App.js
@@ -24,20 +24,3 @@ function App() {
 }
 
 export default App;
-
-
-//function App() {
-//     return (
-//         <div className="App">
-//             <Router>
-//                     <Header/>
-//                 <Switch>
-//                     <Route exact path={'/movies'} component = {MoviesList}/>
-//                     <Route path={`/movies/:id`} component = {Movie}/>
-//                 </Switch>
-//             </Router>
-//         </div>
-//     );
-// }
-//
-// export default App;
